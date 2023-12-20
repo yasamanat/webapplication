68b950591fb78c7a8ce6da2c50dd9b952a57bcc2
Roataee1 <123747495+yasamanat@users.noreply.github.com>
2023-12-20 22:33:28 +0330

    add bluebery

--- img.py
+++ img.py
@@ -1,8 +1,8 @@
 import cv2 as cv   
 import numpy as np  
 import matplotlib.pyplot as plt 
-y=np.array([23,44,56,32])
-mylabe=["apple", "banana" , "mango" , "cherry"]
+y=np.array([25,44,56,32])
+mylabe=["apple", "bluebery" , "mango" , "cherry"]
 myexplod=[-0.5 , 0.3 , 0 ,0]
 plt.pie(y,labels=mylabe , explode=myexplod , shadow=True)
 plt.show()
