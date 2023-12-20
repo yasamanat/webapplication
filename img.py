72cf45a313631278a5c43d18706bf66938f59fe0
Roataee1 <123747495+yasamanat@users.noreply.github.com>
2023-12-20 13:25:49 +0330

    show my array pie.plot

---
+++ img.py
@@ -0,0 +1,8 @@
+import cv2 as cv   
+import numpy as np  
+import matplotlib.pyplot as plt 
+y=np.array([23,44,56,32])
+mylabe=["apple", "banana" , "mango" , "date"]
+myexplod=[-0.5 , 0.3 , 0 ,0]
+plt.pie(y,labels=mylabe , explode=myexplod , shadow=True)
+plt.show()
