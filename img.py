dc806003cfafb904bd3a5cdc715033dd7f23baee
Roataee1 <123747495+yasamanat@users.noreply.github.com>
2023-12-20 13:30:00 +0330

    change fruits

--- img.py
+++ img.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np  
 import matplotlib.pyplot as plt 
 y=np.array([23,44,56,32])
-mylabe=["apple", "banana" , "mango" , "date"]
+mylabe=["apple", "banana" , "mango" , "cherry"]
 myexplod=[-0.5 , 0.3 , 0 ,0]
 plt.pie(y,labels=mylabe , explode=myexplod , shadow=True)
 plt.show()
